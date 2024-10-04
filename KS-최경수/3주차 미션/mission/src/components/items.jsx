e55fae3ee94d0d549554a9897bc6e35bdcb6c74a
choikyungsoo <ryan1954@naver.com>
2024-10-04 17:20:01 +0900

    feat : 3주차 - 홈화면 완성

---
+++ KS-최경수/3주차 미션/mission/src/components/items.jsx
@@ -0,0 +1,49 @@
+import { styled } from 'styled-components';
+import "../../../../0-공통/color.css"
+
+const Items = ({data}) => {
+
+  const movieimgPath = "https://image.tmdb.org/t/p/original" + data.poster_path;
+  const movietitle = data.title;
+  const moviedate = data.release_date;
+
+  console.log(data);
+
+  return (
+    <>
+    <MoviesItem image={movieimgPath}></MoviesItem>
+    <MoviesItemTitle>{movietitle}</MoviesItemTitle>
+    <MoviesItemDate>{moviedate}</MoviesItemDate>
+    </>
+  )
+}
+
+export default Items
+
+const MoviesItem = styled.div`
+  width: 130px;
+  height: 200px;
+  background-image: url(${$props => $props.image});
+  border-radius: 15px;
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  &:hover{
+    filter: brightness(0.5);
+  }
+`
+
+const MoviesItemTitle = styled.p`
+  margin-top: 10px;
+  color: var(--w3-fontColor);
+  font-size: 14px;
+  font-weight: 700;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`
+const MoviesItemDate = styled.p`
+  margin-top: 3px;
+  color: var(--w3-fontColor);
+  font-size: 12px;
+`
--- KS-최경수/3주차 미션/mission/src/pages/home.jsx
+++ KS-최경수/3주차 미션/mission/src/pages/home.jsx
@@ -1,10 +1,54 @@
+import { useEffect, useState } from 'react';
+import axios from "axios";
+import Items from '../components/Items';
+import { styled } from 'styled-components';
+
 const Home = () => {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const getMovies = async () => {
+        const movies = await axios.get(`https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1`, {
+            headers: {
+                Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMDU5YjgxZjNhNzM4ODM1NWY1M2ZjNTdjMDgzMzI5ZSIsIm5iZiI6MTcyNzk1NTc1OC4zNDc4NjksInN1YiI6IjY2ZmU3ZmE0YzlhMTBkNDZlYTdjOGZlNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nKUWbSsB_icBkh1vRdGa61tuXuwXNpjagQsoqLldSN0`,
+            }
+        })
+        setMovies(movies);
+    }
+    getMovies()
+}, []);
 
   return (
     <>
-    <h1>홈입니다</h1>
+      <ItemsContainer>
+        {movies.data?.results.map((movie) => (
+          <>
+            <ItemsWrapper>
+              <Items key={movie.id} data={movie} />
+            </ItemsWrapper>
+          </>
+        ))}
+      </ItemsContainer>
     </>
   )
 }
 
 export default Home;
+
+const ItemsContainer = styled.main`
+  box-sizing: border-box;
+  display: grid;
+  padding: 10px;
+  grid-template-columns: repeat(auto-fit, minmax(134px, auto));
+  gap: 15px;
+
+  > div{
+    background-color: black;
+    width: 130px;
+    height: 240px;
+  }
+`
+
+const ItemsWrapper = styled.div`
+`
