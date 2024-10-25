2d3739bea53fa5cf936d9600c4cb4138212464d2
choikyungsoo <ryan1954@naver.com>
2024-10-25 17:48:18 +0900

    Revert "feat : 5주차 미션 완료"
    This reverts commit d52be54d7b8a10450f7ce2a0b525a3270c15e2db.

--- KS-최경수/5주차 미션/mission/src/pages/login.jsx
+++ KS-최경수/5주차 미션/mission/src/pages/login.jsx
@@ -4,31 +4,16 @@ import * as yup from 'yup'
 import {yupResolver} from '@hookform/resolvers/yup'
 
 const Login = () => {
-  const schema = yup.object().shape({
-    email: yup.string().required("이메일을 입력해주세요").matches(
-      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i,
-      "올바른 이메일 형식이 아닙니다. 다시 확인해주세요"
-    ),
-    password: yup.string().min(8, "비밀번호는 8~16자 사이로 입력해주세요!").max(16, "비밀번호는 8~16자 사이로 입력해주세요!").required(),
-  })
-
-  const {register, handleSubmit, formState: {errors}} = useForm({
-    resolver: yupResolver(schema)
-  });
-
-  const onSubmit = (data) => {
-    console.log(data);
-  }
 
   return (
     <LoginContainer>
     <LoginTitle>로그인</LoginTitle>
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <LoginEmailInput type={'email'}{...register("email")} placeholder='이메일을 입력해주세요!'/>
-      <LoginEamilError>{errors.email?.message}</LoginEamilError>
-      <LoginPasswordInput type={'password'}{...register("password")} placeholder='비밀번호를 입력해주세요!'/>
-      <LoginPasswordError>{errors.password?.message}</LoginPasswordError>
-      <LoginSubmit type={'submit'} value={'로그인'} disabled={errors.email === undefined && errors.password === undefined ? false : true}/>
+    <form>
+      <LoginEmailInput type={'email'} placeholder='이메일을 입력해주세요!'/>
+      <LoginEamilError/>
+      <LoginPasswordInput type={'password'} placeholder='비밀번호를 입력해주세요!'/>
+      <LoginPasswordError/>
+      <LoginSubmit type={'submit'} value={'로그인'}/>
     </form>
     </LoginContainer>
   )
@@ -54,16 +39,8 @@ const InputCommonStyle = css`
   padding-left : 10px;
   box-sizing: border-box;
 
-  &:focus{
+  &:hover{
   }
-`
-
-const ErrorCommonStyle = css`
-  color: var(--w5-errorColor);
-  margin-top: 5px;
-  margin-left: 3px;
-  font-size: 12px;
-}
 `
 
 const LoginEmailInput = styled.input`
@@ -71,18 +48,14 @@ const LoginEmailInput = styled.input`
   margin-top: 35px;
 
 `
-const LoginEamilError = styled.p`
-  ${ErrorCommonStyle}
-`
+const LoginEamilError = styled.p``
 
 const LoginPasswordInput = styled.input`
   ${InputCommonStyle}
   margin-top: 15px;
 `
 
-const LoginPasswordError = styled.p`
-  ${ErrorCommonStyle}
-`
+const LoginPasswordError = styled.p``
 
 
 const LoginSubmit = styled.input`
@@ -90,7 +63,7 @@ const LoginSubmit = styled.input`
   height: 50px;
   border-radius: 5px;
   border: none;
-  background-color: ${props=>props.disabled? 'var(--w5-disabledbuttonColor)': 'var(--w5-buttonColor)'};
+  background-color: var(--w5-buttonColor);
   color: var(--w5-titlecolor);
   margin-top: 15px;
 `
