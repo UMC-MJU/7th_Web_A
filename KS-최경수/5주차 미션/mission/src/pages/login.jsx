d183df0f42b84bb30a84f5b7aaba3819e8f299ba
choikyungsoo <ryan1954@naver.com>
2024-10-25 18:11:41 +0900

    feat : 5주차 실습1 완료

--- KS-최경수/5주차 미션/mission/src/pages/login.jsx
+++ KS-최경수/5주차 미션/mission/src/pages/login.jsx
@@ -4,16 +4,31 @@ import * as yup from 'yup'
 import {yupResolver} from '@hookform/resolvers/yup'
 
 const Login = () => {
+  const schema = yup.object().shape({
+    email: yup.string().required("이메일을 입력해주세요").matches(
+      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i,
+      "올바른 이메일 형식이 아닙니다. 다시 확인해주세요"
+    ),
+    password: yup.string().min(8, "비밀번호는 8~16자 사이로 입력해주세요!").max(16, "비밀번호는 8~16자 사이로 입력해주세요!").required(),
+  })
 
+  const {register, handleSubmit, formState: {errors}} = useForm({
+    resolver: yupResolver(schema)
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  }
+  
   return (
     <LoginContainer>
     <LoginTitle>로그인</LoginTitle>
-    <form>
-      <LoginEmailInput type={'email'} placeholder='이메일을 입력해주세요!'/>
-      <LoginEamilError/>
-      <LoginPasswordInput type={'password'} placeholder='비밀번호를 입력해주세요!'/>
-      <LoginPasswordError/>
-      <LoginSubmit type={'submit'} value={'로그인'}/>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <LoginEmailInput type={'email'}{...register("email")} placeholder='이메일을 입력해주세요!'/>
+      <LoginEamilError>{errors.email?.message}</LoginEamilError>
+      <LoginPasswordInput type={'password'}{...register("password")} placeholder='비밀번호를 입력해주세요!'/>
+      <LoginPasswordError>{errors.password?.message}</LoginPasswordError>
+      <LoginSubmit type={'submit'} value={'로그인'} disabled={errors.email === undefined && errors.password === undefined ? false : true}/>
     </form>
     </LoginContainer>
   )
@@ -31,6 +46,7 @@ const LoginTitle = styled.p`
   color: var(--w5-titlecolor);
   font-weight: bold;
   font-size: 30px;
+
 `
 const InputCommonStyle = css`
   width: 380px;
@@ -39,8 +55,16 @@ const InputCommonStyle = css`
   padding-left : 10px;
   box-sizing: border-box;
 
-  &:hover{
+  &:focus{
   }
+`
+
+const ErrorCommonStyle = css`
+  color: var(--w5-errorColor);
+  margin-top: 5px;
+  margin-left: 3px;
+  font-size: 12px;
+}
 `
 
 const LoginEmailInput = styled.input`
@@ -48,14 +72,18 @@ const LoginEmailInput = styled.input`
   margin-top: 35px;
 
 `
-const LoginEamilError = styled.p``
+const LoginEamilError = styled.p`
+  ${ErrorCommonStyle}
+`
 
 const LoginPasswordInput = styled.input`
   ${InputCommonStyle}
   margin-top: 15px;
 `
 
-const LoginPasswordError = styled.p``
+const LoginPasswordError = styled.p`
+  ${ErrorCommonStyle}
+`
 
 
 const LoginSubmit = styled.input`
@@ -63,7 +91,7 @@ const LoginSubmit = styled.input`
   height: 50px;
   border-radius: 5px;
   border: none;
-  background-color: var(--w5-buttonColor);
+  background-color: ${props=>props.disabled? 'var(--w5-disabledbuttonColor)': 'var(--w5-buttonColor)'};
   color: var(--w5-titlecolor);
   margin-top: 15px;
 `
