4ae325905ef27863e434cde9a1bb766259f129dc
kingmingseo <101965138+kingmingseo@users.noreply.github.com>
2024-11-05 22:02:23 +0900

    Revert "Merge pull request #9 from UMC-MJU/밍붕"
    This reverts commit 9a23dae3e1f188cd8d096d3f0345ebb722e6159a, reversing
    changes made to d760e511d63a3562832c9cadd4c8062da5e55e1a.

--- 밍붕-김민서/4주차+5주차 미션 밍붕/src/assets/index.js
+++
@@ -1,5 +0,0 @@
-export { default as cbr } from './cbr500r.png';
-export { default as ninja } from './ninja.png';
-export { default as r1 } from './r1.png';
-export { default as rocket } from './rocket.png';
-export { default as watcha } from './watcha.png';
