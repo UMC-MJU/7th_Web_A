d276273427b1d9a9b6f894af260ad25edc21c884
choikyungsoo <ryan1954@naver.com>
2024-11-09 18:25:48 +0900

    feat: 6주차 미션 2. 검색 UI 구현 완료

--- KS-최경수/6주차 미션/mission/src/pages/search.jsx
+++ KS-최경수/6주차 미션/mission/src/pages/search.jsx
@@ -1,10 +1,27 @@
+import { styled } from 'styled-components';
+
 const Search = () => {
 
   return (
     <>
-    <h1>검색</h1>
+    <SearchInput placeholder='영화 제목을 입력해주세요...'></SearchInput>
+    <SearchButton type={'submit'} value={'검색'} >검색</SearchButton>
     </>
   )
 }
 
 export default Search
+
+const SearchInput = styled.input`
+  width: calc(100% - 97px);
+  border: none;
+  padding: 15px 0px 15px 12px;
+  border-radius: 5px 0px 0px 5px;
+`
+const SearchButton = styled.button`
+  border: none;
+  border-radius: 0px 5px 5px 0px;
+  padding: 15px 30px;
+  color : var(--w6-textcolor);
+  background-color: var(--w6-buttonColor);
+`
