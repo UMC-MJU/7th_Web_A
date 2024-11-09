3f4e4f721148e24a2f1cc19a0db90c2435d61abf
choikyungsoo <ryan1954@naver.com>
2024-11-09 21:50:32 +0900

    refactor : 6주차 axios insatance value화

--- KS-최경수/6주차 미션/mission/src/apis/axios-user.js
+++ KS-최경수/6주차 미션/mission/src/apis/axios-user.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
+const token = localStorage.getItem("accessToken");
+
 const userInstance = axios.create({
   headers :{
-    Authorization:`Bearer ${localStorage.getItem("accessToken")}`
+    Authorization:`Bearer ${token}`
   },
   baseURL : import.meta.env.VITE_USER_API_URL
 })
+
 
 export {userInstance}
