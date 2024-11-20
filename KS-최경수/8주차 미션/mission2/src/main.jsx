94a7f407fb4d6976c8fbd133591144b05b4521c2
choikyungsoo <ryan1954@naver.com>
2024-11-20 19:13:42 +0900

    feat: 8주차 mission2 완료

--- KS-최경수/8주차 미션/mission2/src/main.jsx
+++ KS-최경수/8주차 미션/mission2/src/main.jsx
@@ -2,7 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+export const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />  
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
 )
